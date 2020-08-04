4357e92eb76716e1b835da02a292f9e34a5d3bde
aintme <tomasz.jurkiewicz@capgemini.com>
2020-08-04 10:58:50 +0200

    build app

---
+++ build/precache-manifest.34786d6ae0e894688708a7a6d149ab74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f6543e4ac376056841802b0e0192dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a38dc84a21bf83fc1aa",
+    "url": "/static/css/main.d61d4935.chunk.css"
+  },
+  {
+    "revision": "c5ec6695c5419886a74e",
+    "url": "/static/js/2.6fab4bb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fab4bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a38dc84a21bf83fc1aa",
+    "url": "/static/js/main.ecb2409c.chunk.js"
+  },
+  {
+    "revision": "e0a4deefcd93d87cbe5a",
+    "url": "/static/js/runtime-main.a22a01e6.js"
+  },
+  {
+    "revision": "b9a6b0858cec5fbc678f6de3dab68449",
+    "url": "/static/media/afternoon.b9a6b085.jpg"
+  },
+  {
+    "revision": "b1a03f074c0f639908a7e4fb7b373fc0",
+    "url": "/static/media/error.b1a03f07.png"
+  },
+  {
+    "revision": "85d922dbc00e119724be287b9f7bf32d",
+    "url": "/static/media/morning.85d922db.jpg"
+  },
+  {
+    "revision": "214a2c1183ece972d39238a6258598d1",
+    "url": "/static/media/night.214a2c11.jpg"
+  }
+]);
